672e1e2d9eecb93fd210df227f1f5a84ceaa9a70
Amit Bhattacharjee <a.k.bhattacharjee@gmail.com>
2019-09-06 12:25:28 +0530

    Add files via upload

---
+++ codes/3rdYear/09_primefactor.c
@@ -0,0 +1,81 @@
+/* Registration: xxxx; 
+   Description: Code for finding factors and prime factors of a number
+   Author: AKB */
+
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+
+  /* Type declaration */
+  int n,N,i,j,prod=1,add=0;
+  
+  /* Input the number */
+  printf("Enter the number: ");
+  scanf("%d", &n); N=n;  /* store the number for future use */
+
+  /* Factorize the number */
+  printf("Factors of %d are ",n);
+  for(i=1; i<=n; i++){
+     if(n%i == 0) printf("%d ",i);    
+  } 
+
+  /* Prime factor: Every composite number has at least one prime factor less than equals square root of itself */
+  printf("\nPrime factors of %d are ", n);
+  for(i=2; i<=sqrt(n); i++){
+     if(n%i == 0){
+       printf("%d ",i);   
+       add += i; 
+       n = n/i;
+       i--;
+     }
+  }
+  if(n!=1){
+    printf("%d ",n);
+    add += n; 
+  }
+  printf("\nSum of prime factors of %d is %d \n", N, add);
+  
+  /* Product of only prime factors (without repetition) */
+  n = N; j = 2; 
+  for(i=2; i<=sqrt(n); i++){
+     if(n%i == 0){
+        if(i%j != 0){ 
+           j     = i;   /* Update j with new i, so that one prime is counted only once */
+           prod *= i;
+        }
+        n = n/i;
+        i--;
+     }
+  }
+  if(n!=1){
+     if(n%j!=0){        /* n is prime, but if equal to j then its double counting */ 
+        prod *= n;
+     }
+  }
+  if(N%2==0) printf("Product of prime factors of %d are %d\n", N, 2*prod); /* Multiply 2 because its never counted */
+  else       printf("Product of prime factors of %d are %d\n", N,   prod);
+  
+return 0;
+}
+
+/* Results 
+Enter the number: 4158
+Factors of 4158 are 1 2 3 6 7 9 11 14 18 21 22 27 33 42 54 63 66 77 99 126 154 189 198 231 297 378 462 594 693 1386 2079 4158 
+Prime factors of 4158 are 2 3 3 3 7 11
+Sum of prime factors is 29 
+Product of prime factors of 4158 are 462
+
+Enter the number: 168
+Factors of 168 are 1 2 3 4 6 7 8 12 14 21 24 28 42 56 84 168 
+Prime factors of 168 are 2 2 2 3 7
+Sum of prime factors is 16 
+Product of prime factors of 168 are 42
+
+Enter the number: 30030
+Factors of 30030 are 1 2 3 5 6 7 10 11 13 14 15 21 22 26 30 33 35 39 42 55 65 66 70 77 78 91 105 110 130 143 154 165 182 195 210 
+231 273 286 330 385 390 429 455 462 546 715 770 858 910 1001 1155 1365 1430 2002 2145 2310 2730 3003 4290 5005 6006 10010 15015 30030 
+Prime factors of 30030 are 2 3 5 7 11 13 
+Sum of prime factors is 41 
+Product of prime factors of 30030 are 30030.
+*/
---
+++ codes/3rdYear/10_primerange.c
@@ -0,0 +1,74 @@
+/* Registration Number: xxxx; 
+   Description: Code for finding prime numbers within a range. 
+                Note that 25 primes are there within [0,1e2],
+                         168 primes are there within [1,1e3],
+                        1229 primes are there within [1,1e4], 
+                        9592 primes are there within [1,1e5], 
+                       78498 primes are there within [1,1e6] and so on 
+   Author: AKB */
+
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+
+  /* Type declaration */
+  int u,l,i,j,flag,count=0,add=0;
+  int num=4,prime[100000]; /* array size > number of primes */ 
+  
+  printf("Enter the upper and lower limit: ");
+  scanf("%d %d", &l, &u);
+  printf("The prime numbers within this range is\n");
+
+  /* Do the iteration */
+  for(i=l;i<=u;i++){
+     flag = 0;
+     for(j=2;j<=sqrt(i);j++){
+        if(i%j==0){
+          flag = 1;
+          break;
+        }
+     }
+     if(i>1 && flag==0){
+        printf("%d ",i);
+        count++;           /* Count how many primes are there */
+        prime[count] = i;  /* Store prime numbers in an array */
+     }
+  } 
+  printf("\nThere are %d primes in this range\n", count);
+  
+  /* Compute addition of few prime numbers */
+  if (count >= num){
+     add=0;  /* forward */
+     for(i=1; i<=num; i++) add += prime[i];
+     printf("Addition of first %d primes within this range is %d\n", num, add);
+  
+     add=0;  /* reverse */
+     for(i=count; i>=count-num+1; i--) add += prime[i];
+     printf("Addition of last %d primes within this range is %d\n", num, add);
+  }
+return 0;
+}
+
+/* Results: 
+Enter the two limits: 10 50
+The prime factors within the range is
+11 13 17 19 23 29 31 37 41 43 47
+There are 11 primes in this range
+Addition of first 4 primes within this range is 60
+Addition of last 4 primes within this range is 168
+
+Enter the two limits: 1 100
+The prime factors within the range is
+1 2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97
+There are 26 primes in this range
+Addition of first 4 primes within this range is 11
+Addition of last 4 primes within this range is 348
+
+Enter the upper and lower limit: 1 1000
+The prime numbers within this range is
+1 2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97 101 103 107 109 113 127 131 137 139 149 151 157 163 167 173 179 181 191 193 197 199 211 223 227 229 233 239 241 251 257 263 269 271 277 281 283 293 307 311 313 317 331 337 347 349 353 359 367 373 379 383 389 397 401 409 419 421 431 433 439 443 449 457 461 463 467 479 487 491 499 503 509 521 523 541 547 557 563 569 571 577 587 593 599 601 607 613 617 619 631 641 643 647 653 659 661 673 677 683 691 701 709 719 727 733 739 743 751 757 761 769 773 787 797 809 811 821 823 827 829 839 853 857 859 863 877 881 883 887 907 911 919 929 937 941 947 953 967 971 977 983 991 997 
+There are 169 primes in this range
+Addition of first 4 primes within this range is 11
+Addition of last 4 primes within this range is 3948
+*/
